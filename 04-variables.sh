38c42a12517e43cf2db992ecdeb8134fb4d9175a
julurushankar90 <julurushankar90@gmail.com>
2026-08-14 15:06:25 +0530

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 # no space between value and =
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, Iam fine thanks for asking. How are you doing?"
+echo "$PERSON1:: Doing fine. What are you leaning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
