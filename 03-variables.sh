1022cc2354853eac4d20a62ede1366f5c16a8bff
julurushankar90 <julurushankar90@gmail.com>
2026-08-14 14:38:13 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,3 +7,4 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, Iam fine thanks for asking. How are you doing?"
 echo "$PERSON1:: Doing fine. What are you leaning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
+
