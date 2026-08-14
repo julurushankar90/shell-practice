0ec18444042053a0e3aacb0ac2f2a04657491e3a
julurushankar90 <julurushankar90@gmail.com>
2026-08-14 13:35:48 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh:: Hello Ramesh, How are you?"
+echo "ramesh:: Hi suresh, Iam fine thanks for asking. How are you doing?"
+echo "suresh:: Doing fine. What are you leaning Ramesh?"
+echo "Ramesh:: I am learning Shell. What about you?"
