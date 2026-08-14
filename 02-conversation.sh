bc83855ab2352e63ecc6d95d3e003d3188ff3f1d
julurushankar90 <julurushankar90@gmail.com>
2026-08-14 14:08:00 +0530

    actions

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,3 +4,4 @@ echo "suresh:: Hello Ramesh, How are you?"
 echo "ramesh:: Hi suresh, Iam fine thanks for asking. How are you doing?"
 echo "suresh:: Doing fine. What are you leaning Ramesh?"
 echo "Ramesh:: I am learning Shell. What about you?"
+
